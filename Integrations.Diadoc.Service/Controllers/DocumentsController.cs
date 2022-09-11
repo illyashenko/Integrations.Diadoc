1b539e7b129624f6ccbbf296a86c4ee5d958223a
illyashenko_iv <illyashenko_iv@OFFICE-IT-78>
2022-09-11 20:34:36 +0300

    #11_09

---
+++ Integrations.Diadoc.Service/Controllers/DocumentsController.cs
@@ -0,0 +1,12 @@
+﻿using MassTransitRMQExtensions.Attributes.JobAttributes;
+
+namespace Integrations.Diadoc.Service.Controllers;
+
+public class DocumentsController
+{
+    [RunJob("1/5 * * * * ?")]
+    public void Start()
+    {
+        Console.WriteLine("Test!!");
+    }
+}
--- Integrations.Diadoc.Service/Program.cs
+++ Integrations.Diadoc.Service/Program.cs
@@ -1,5 +1,22 @@
+using MassTransitRMQExtensions;
+using MassTransitRMQExtensions.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var rabbitConfig = builder.Configuration.GetSection("Rabbit");
+var mqConfig = new RabbitMqConfig(rabbitConfig["UserName"], rabbitConfig["Password"],
+    new Uri($"amqp://{rabbitConfig["HostName"]}:{rabbitConfig["Port"]}"));
+
+builder.Services.ConfigureMassTransitControllers(mqConfig);
+
 var app = builder.Build();
 
+app.MapGet("/About", context =>
+    context.Response.WriteAsJsonAsync(new
+    {
+        Description = "Integration.Diadoc - Интеграция с Контур.Диадок",
+        Environment = app.Environment.EnvironmentName,
+        Version = typeof(Program).Assembly.GetName().Version?.ToString()
+    }));
 
 app.Run();
